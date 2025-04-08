59cc710e225aa5ca35b9c9543a9553f196f932b9
aseel <aseel.huneidi@polaris-tek.com>
2025-04-08 20:20:15 +0300

    fix: fix run error

--- application_details/views.py
+++ application_details/views.py
@@ -7,7 +7,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import application_details
+from .models import Application
 from .serializers import ApplicationSerializer
 
 
