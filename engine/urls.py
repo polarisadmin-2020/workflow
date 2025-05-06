c725f64c4da4819b703fbc633514cbb3057d2ad3
Sadeq <sadeq.alsugha@polaris-tek.com>
2025-05-06 09:17:11 +0300

    BALAD-1495 Get steps by WF id and update delete step by id

--- engine/urls.py
+++ engine/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from .views import StepCreateView, WorkflowCreateView
+from engine.views import StepCreateView, StepDeleteView, StepUpdateView, StepsByWorkflowView, WorkflowCreateView, WorkflowUpdateView
 
 urlpatterns = [
     # Workflow endpoints
-    path("workflows/create/", WorkflowCreateView.as_view(), name="workflow-create"),
+    path('workflows/create/', WorkflowCreateView.as_view(), name='workflow-create'),
+    path('workflows/<int:pk>/update/', WorkflowUpdateView.as_view(), name='workflow-update'),
+
     # Step endpoints
-    path("steps/create/", StepCreateView.as_view(), name="step-create"),
+    path('steps/create/', StepCreateView.as_view(), name='step-create'),
+    path('steps/<int:pk>/update/', StepUpdateView.as_view(), name='step-update'),
+    path('steps/<int:pk>/delete/', StepDeleteView.as_view(), name='step-delete'),
+    path('workflows/<int:workflow_id>/steps/', StepsByWorkflowView.as_view(), name='workflow-steps'),
 ]
