e589b42241806036cf23c8c0d68b828e52312cf8
Sadeq <sadeq.alsugha@polaris-tek.com>
2025-07-09 15:04:13 +0300

    make the subservice just required for create

--- engine/serializers.py
+++ engine/serializers.py
@@ -2,6 +2,24 @@ from rest_framework import serializers
 
 from engine.models import Action, Step, Workflow
 
+class WorkflowCreateSerializer(serializers.ModelSerializer):
+    code = serializers.CharField(required=False, allow_blank=True)
+    name_ar = serializers.CharField(required=False, allow_blank=True)
+    name_en = serializers.CharField(required=False, allow_blank=True)
+
+    class Meta:
+        model = Workflow
+        fields = [
+            "id",
+            "code",
+            "name_ar",
+            "name_en",
+            "subservice_id",
+            "active",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["created_at", "updated_at"]
 
 class WorkflowSerializer(serializers.ModelSerializer):
     class Meta:
